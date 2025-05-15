9cf2993f2dc3f2c9cb1e7c280e8bb271cfbdc114
Iheb CHAFAI <47852295+ihebcap@users.noreply.github.com>
2025-05-15 17:31:06 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/ich/', // 👈 ajoute cette ligne
   server: {
     host: "::",
     port: 8080,
